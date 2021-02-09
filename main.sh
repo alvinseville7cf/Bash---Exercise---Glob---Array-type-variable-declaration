93fce51446e77ac4d0ddb8c666272dff59c39ffa
Alvin Seville <alvinseville7cf@gmail.com>
2021-02-09 14:35:52 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,21 @@
+list=("var x = new int[1] { 1 }" "var x = new int[1] { 1, }" "var x = new int[1] { 1, 2 }")
+
+integerNumberGlob='?(-|+)+([[:digit:]])?(u|ul|lu)'
+floatingNumberGlob='?(-|+)+([[:digit:]])?(.+([[:digit:]]))?(d|f|m)'
+charGlob="'?'"
+stringGlob='"*"'
+nullGlob='null'
+valueGlob="@($integerNumberGlob|$floatingNumberGlob|$charGlob|$stringGlob|$nullGlob)"
+
+initializerGlob="{*(*([[:space:]])$valueGlob*([[:space:]]),)?(*([[:space:]])$valueGlob*([[:space:]]))}"
+
+identifierGlob='[[:alpha:]]*([[:alnum:]])'
+typeGlob="$identifierGlob"
+
+glob="$typeGlob+([[:space:]])$identifierGlob*([[:space:]])=*([[:space:]])new+([[:space:]])$typeGlob*([[:space:]])\[*([[:space:]])$integerNumberGlob*([[:space:]])\]*([[:space:]])?($initializerGlob)"
+
+shopt -s nocasematch
+for i in "${list[@]}"
+do
+  [[ "$i" == $glob ]] && echo $i || continue
+done
